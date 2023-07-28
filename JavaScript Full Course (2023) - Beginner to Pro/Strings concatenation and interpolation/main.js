c4eef5a17c7deb0036afbf781d31b17d2b2960fb
Joicemar <bboygutela@gmail.com>
2023-07-28 18:25:15 -0300

    exercise

--- JavaScript Full Course (2023) - Beginner to Pro/Strings concatenation and interpolation/main.js
+++ JavaScript Full Course (2023) - Beginner to Pro/Strings concatenation and interpolation/main.js
@@ -4,3 +4,6 @@ let result = `items (${1 + 1}: valor $${(2095 + 799) / 100})`;
 
 var meuElemento = document.querySelector('div');
 meuElemento.textContent = result;
+
+//exercise:
+alert(`value: ${20+19.40} \n10% tax: ${(20+19.40)*0.10} `)
--- JavaScript Full Course (2023) - Beginner to Pro/Strings simple and double quotes/main.js
+++ JavaScript Full Course (2023) - Beginner to Pro/Strings simple and double quotes/main.js
@@ -1,5 +1,9 @@
 
 
-'single quotes'
+'Use single quotes by default'
 "'double quotes represent single quotes in result'"
-`back ticks is the way to use math and strings together ${2+2}`
+`If we need interpolation, multi-line strings use `` 
+back ticks is the way to use math and strings together ${2+2}`
+
+//practice
+alert('Value: `${}`' );

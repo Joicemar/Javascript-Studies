b8e22716cdc39cb65c7fee61d086c7768d8056d5
Joicemar <bboygutela@gmail.com>
2023-07-27 23:53:45 -0300

    Start JS

---
+++ JavaScript Full Course (2023) - Beginner to Pro/Strings concatnation and interpolation/main.js
@@ -0,0 +1,6 @@
+
+//Concatenation e interpolation
+let result = `items (${1 + 1}: valor $${(2095 + 799) / 100})`;
+
+var meuElemento = document.querySelector('div');
+meuElemento.textContent = result;

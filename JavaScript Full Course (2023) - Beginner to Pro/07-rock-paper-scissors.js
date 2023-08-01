f9a11b3c16efafec4f565d326f48d6cf40e2c165
Joicemar <bboygutela@gmail.com>
2023-08-01 20:46:34 -0300

    Update game
    Seção de functions terminada.

--- JavaScript Full Course (2023) - Beginner to Pro/07-rock-paper-scissors.js
+++ JavaScript Full Course (2023) - Beginner to Pro/07-rock-paper-scissors.js
@@ -1,7 +1,46 @@
-let result = '';
-function pickComputerMove(){
-    const randomNumber = Math.random();
+/**
+ * 
+ *
+ */
+function playGame(playerMove) {
+    const computerMove = pickComputerMove();
 
+    let result = '';
+    if (playerMove === 'Scissors') {
+        if (computerMove === 'rock') {
+            result = 'You lose.';
+        } else if (computerMove === 'paper') {
+            result = 'You win.';
+        } else if (computerMove === 'scissors') {
+            result = 'Tie.';
+        }
+        alert(`You picked scissors. Computer picked ${computerMove}. ${result}`);
+    }
+    if (playerMove === 'Paper') {
+        if (computerMove === 'rock') {
+            result = 'You win.';
+        } else if (computerMove === 'paper') {
+            result = 'Tie.';
+        } else if (computerMove === 'scissors') {
+            result = 'You lose.';
+        }
+        alert(`You picked paper. Computer picked ${computerMove}. ${result}`);
+    }
+    if (playerMove === 'Rock') {
+        if (computerMove === 'rock') {
+            result = 'Tie.';
+        } else if (computerMove === 'paper') {
+            result = 'You lose.';
+        } else if (computerMove === 'scissors') {
+            result = 'You win.';
+        }
+        alert(`You picked paper. Computer picked ${computerMove}. ${result}`);
+    }
+}
+
+function pickComputerMove() {
+    const randomNumber = Math.random();
+    greet();
     if (randomNumber >= 0 && randomNumber < 1 / 3) {
         return 'rock';
     } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
@@ -12,4 +51,14 @@ function pickComputerMove(){
     /*Uma das principais vantagens de usar variaveis em escopo de funções é que
     pode ser nomeadas com o mesmo nome de outro arquivo de código
     */
+
+}
+let Simom = 'Simom';
+let Joe = 'Joe';
+let Doe = 'Doe';
+function greet(name){
+    if(name === undefined){
+        console.log('Hi there!')
+    }
+   console.log(`Hello ${Simom}`)
 }

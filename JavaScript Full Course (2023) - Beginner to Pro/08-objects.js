831a215f9e19aedf310b9ff2c78cc4e079f45fc1
Joicemar <bboygutela@gmail.com>
2023-08-06 11:58:42 -0300

    classes

--- JavaScript Full Course (2023) - Beginner to Pro/08-objects.js
+++ JavaScript Full Course (2023) - Beginner to Pro/08-objects.js
@@ -6,3 +6,13 @@ const product = {
     price: 1090
 }
 console.log(product);
+console.log(product.name);
+console.log(product.price);
+
+product.name = 'cotton sockts';
+console.log(product);
+
+product.newProperty = "tere";
+product.newProperty = true;
+console.log(product) 
+console.log(product.newProperty) 
